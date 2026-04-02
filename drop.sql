1f018a5fa4e0014de483a74699a0b80a47998c53
Valentin Nyzam <valentin.nyzam@gmail.com>
2026-04-02 13:33:54 +0200

    Remove drop.sql, add TODO and coverage report

--- drop.sql
+++
@@ -1,6 +0,0 @@
--- Supprimer toutes les tables, fonctions, types, vues
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-
--- Réappliquer les grants par défaut
-GRANT ALL ON SCHEMA public TO postgres;
